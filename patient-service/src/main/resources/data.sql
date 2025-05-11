a7732159668590c676e5604297fded7f474fe700
Aniket Sharan <sarananiketgupta1610@gmail.com>
2025-05-11 13:25:23 +0530

    add docker fixes

--- patient-service/src/main/resources/data.sql
+++ patient-service/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 -- Ensure the 'patient' table exists
 CREATE TABLE IF NOT EXISTS patient
 (
-    id              UUID PRIMARY KEY,
+    id              VARCHAR(36) PRIMARY KEY,
     patient_name            VARCHAR(255)        NOT NULL,
     email           VARCHAR(255) UNIQUE NOT NULL,
     address         VARCHAR(255)        NOT NULL,
